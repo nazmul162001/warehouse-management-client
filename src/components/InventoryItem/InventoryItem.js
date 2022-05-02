df08823adfe8ba59705466bc4d4bb38d0bafa4ca
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-02 15:05:48 +0600

    added our speciality section

--- src/components/InventoryItem/InventoryItem.js
+++ src/components/InventoryItem/InventoryItem.js
@@ -1,29 +1,34 @@
 import React from 'react';
+import './InventoryItem.css';
 
 const InventoryItem = (props) => {
   const { name, description, price, supplier, img, quantity } = props.item;
   return (
-    <div className="rounded-md p-5 shadow-md shadow-gray-400">
-      <img className="rounded-xl mb-2" src={img} alt="" />
-      <p>
-        <strong>{name}</strong>
-      </p>
-      <p>Price: ${price}</p>
-      <p>
-        Quantity:{' '}
-        <span
-          style={{ backgroundColor: 'rgba(255, 0, 0, 0.20)' }}
-          className="py-1 px-3 rounded-full"
-        >
-          {quantity}
-        </span>{' '}
-      </p>
-      <p className="my-2 text-gray-500"> {description} </p>
-      <p className="italic my-2"> Supplier: {supplier} </p>
-      <div className="text-center">
-        <button className='bg-slate-700 w-full py-2 px-5 rounded-md text-white'>Stock Update</button>
+    <>
+      <div className="rounded-md p-5 custom-shadow mt-20">
+        <img className="rounded-xl mb-2" src={img} alt="" />
+        <p>
+          <strong>{name}</strong>
+        </p>
+        <p>Price: ${price}</p>
+        <p>
+          Quantity:{' '}
+          <span
+            style={{ backgroundColor: 'rgba(255, 0, 0, 0.20)' }}
+            className="py-1 px-3 rounded-full"
+          >
+            {quantity}
+          </span>{' '}
+        </p>
+        <p className="my-2 text-gray-500"> {description} </p>
+        <p className="italic my-2"> Supplier: {supplier} </p>
+        <div className="text-center">
+          <button className="bg-slate-700 w-full py-2 px-5 rounded-md text-white">
+            Stock Update
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/Speciality/Speciality.js
+++ src/components/Speciality/Speciality.js
@@ -1,9 +1,76 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Speciality.css';
+import { FaArrowRight } from 'react-icons/fa';
 
 const Speciality = () => {
   return (
-    <div>
-      <h1 className="text-5xl text-center w-full h-[200px] bg-green-500 mt-1">Our Speciality </h1>
+    <div className="our-speciality px-16">
+      <div className="see-more  py-4 mr-14 flex items-center justify-end">
+        <Link to='/' className='flex items-center hover:text-orange-500'>
+          {' '}
+          <p className="pr-3 text-xl" to="/">
+            See All Inventories{' '}
+          </p>
+          <FaArrowRight />
+        </Link>
+      </div>
+      <h1 className="text-5xl text-center w-full  my-10">Our Speciality </h1>
+      <div className="food-content grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="food-item">
+          <img
+            className="rounded-xl"
+            src="https://www.kellogggarden.com/wp-content/uploads/2016/07/Bountiful-Harvest-e1490218936705.jpg"
+            alt=""
+          />
+          <h2 className="text-2xl py-3">Natural Radiance</h2>
+          <p className="text-gray-500">
+            A regulatory framework is most important when consumers and farmers
+            are geographically separated, and such a framework is likely to
+            cater to larger-scale producers who participate in a more industrial
+            system. This regulatory approach does not necessarily match
+            consumers’ assumptions about organic food production, which
+            typically include images of small family farms and the humane
+            treatment of animals.
+          </p>
+        </div>
+        <div className="food-item">
+          <img
+            className="rounded-xl"
+            src="https://media-exp1.licdn.com/dms/image/C4E12AQGfpTZRLTLLkA/article-cover_image-shrink_720_1280/0/1634835177119?e=2147483647&v=beta&t=qRyRzxImCCThsXwpMsovUN4IoweNAH0mFl89ztGgEug"
+            alt=""
+          />
+          <h2 className="text-2xl py-3">Healthy Fridge</h2>
+          <p className="text-gray-500">
+            Healthy eating would be a lot easier if someone would clean out the
+            refrigerator, get rid of the junk, and stock the shelves with
+            nutritious choices. If high-fat, high-salt, low-fiber foods aren't
+            in sight (Chubby Hubby, anyone?), they are more likely to be out of
+            mind―and out of mouth. But until you find a
+            nutritionist-slash―personal assistant to do the job for you, take a
+            peek into this healthy refrigerator. Look at it again before you
+            head to the supermarket―it might keep you away from the Cool Whip.
+          </p>
+        </div>
+        <div className="food-item">
+          <img
+            className="rounded-xl"
+            src="https://miro.medium.com/max/1400/1*yBF7ZSAbDxPmm6W4D6M2Sw.jpeg"
+            alt=""
+          />
+          <h2 className="text-2xl py-3">Bountiful Harvest</h2>
+          <p className="text-gray-500">
+            Sales of both seed packets and small transplant-sized starts of
+            tomatoes, peppers, cucumbers, squash and other summer vegetables
+            soared this spring as stay-at-home and safe-at-home guidelines
+            changed the way we all live and work. Sheltering in place turned out
+            to be the inspiration for so many new vegetable gardens that the
+            National Garden Bureau, which is celebrating 100 years of promoting
+            home gardening, launched a modern Victory Garden campaign based on
+            the World War II backyard-gardening initiative.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };

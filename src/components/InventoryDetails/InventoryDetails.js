6458416d1f3fc51fc826d0ecb2a997e0d74a3d7d
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-05 16:36:37 +0600

    added product description

--- src/components/InventoryDetails/InventoryDetails.js
+++ src/components/InventoryDetails/InventoryDetails.js
@@ -3,19 +3,110 @@ import { useParams } from 'react-router-dom';
 
 const InventoryDetails = () => {
   const [item, setItem] = useState({});
+  const { name, img, price, description, supplier, quantity } = item;
+  const { id } = useParams();
 
-  const {id} = useParams();
-
-  useEffect(()=> {
+  useEffect(() => {
     fetch(`http://localhost:5000/service/${id}`)
-    .then(res => res.json())
-    .then(data => setItem(data))
-  },[id])
-  
-  
+      .then((res) => res.json())
+      .then((data) => setItem(data));
+  }, [id]);
+
   return (
-    <div>
-      <h1>This is details page of inventory {item.name} </h1>
+    <div className="w-full h-auto overflow-x-hidden">
+      <div className="flex flex-col">
+        <div className="overflow-x-hidden sm:-mx-6 lg:-mx-8">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full border text-center">
+                <tbody className='grid grid-cols-1 md:grid-cols-2'>
+                  <tr className="border-b">
+                    <td
+                      // colspan="2"
+                      rowspan="3"
+                      className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center border-r w-1/2"
+                    >
+                      <div className="product-img flex justify-center items-center">
+                        <img
+                        className='h-96'
+                         src={img} alt="" />
+                      </div>
+                    </td>
+                  </tr>
+                  <div className="product-details">
+                    <tr className="bg-white border-b">
+                      <td
+                        // colspan="2"
+                        className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center border-r h-auto w-52"
+                      >
+                        Name:
+                      </td>
+                      <td className="text-left text-sm text-gray-900 font-light px-6 py-4 ">
+                        {name}
+                      </td>
+                    </tr>
+                    <tr className="bg-white border-b">
+                      <td
+                        // colspan="2"
+                        className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center border-r h-auto w-52"
+                      >
+                        Price:
+                      </td>
+                      <td className="text-left text-sm text-gray-900 font-light px-6 py-4 ">
+                        {price}
+                      </td>
+                    </tr>
+                    <tr className="bg-white border-b">
+                      <td
+                        // colspan="2"
+                        className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center border-r h-auto w-52"
+                      >
+                        Quantity:
+                      </td>
+                      <td className="text-left text-sm text-gray-900 font-light px-6 py-4 ">
+                        {quantity}
+                      </td>
+                    </tr>
+                    <tr className="bg-white border-b">
+                      <td
+                        // colspan="2"
+                        className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center border-r h-auto w-52"
+                      >
+                        Description:
+                      </td>
+                      <td className="text-left text-sm text-gray-900 font-light px-6 py-4 ">
+                        {description}
+                      </td>
+                    </tr>
+                    <tr className="bg-white border-b">
+                      <td
+                        // colspan="2"
+                        className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center border-r h-auto w-52"
+                      >
+                        Supplier:
+                      </td>
+                      <td className="text-left text-sm text-gray-900 font-light px-6 py-4 ">
+                        {supplier}
+                      </td>
+                    </tr>
+                    <tr className="bg-white border-b">
+                      <td
+                        // colspan="2"
+                        className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center border-r h-auto w-52"
+                      >
+                        Sold:
+                      </td>
+                      <td className="text-left text-sm text-gray-900 font-light px-6 py-4">
+                        Sold
+                      </td>
+                    </tr>
+                  </div>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -7,62 +7,37 @@ import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 // import './styles.css';
 // import required modules
-import { EffectCoverflow, Pagination } from 'swiper';
+import { EffectCoverflow, FreeMode, Pagination } from 'swiper';
 import './Review.css';
 
 const Review = () => {
   return (
     <div>
-      <h1 className="text-center text-5xl w-full h-[200px] bg-green-500 mt-1">
+      <h1 className="text-center text-5xl w-full mt-1">
         What Our Customer Say
       </h1>
 
       {/* swipper */}
       <Swiper
-        effect={'coverflow'}
-        // rewind={true}
-        loop={true}
-        grabCursor={true}
-        centeredSlides={true}
         slidesPerView={3}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
+        spaceBetween={20}
+        freeMode={true}
+        loop={true}
+        pagination={{
+          clickable: true,
         }}
-        pagination={false}
-        modules={[EffectCoverflow, Pagination]}
+        modules={[FreeMode, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <img alt="/" src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt="/" src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt="/" src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt="/" src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt="/" src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt="/" src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt="/" src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt="/" src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt="/" src="https://swiperjs.com/demos/images/nature-9.jpg" />
-        </SwiperSlide>
+        <SwiperSlide><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam omnis voluptas suscipit eum iure laborum velit temporibus tenetur quas. Exercitationem fugit nemo laudantium quas saepe magnam totam corrupti a facilis molestiae, alias aliquam, iste labore nihil quo atque aspernatur repellat laborum ex voluptas voluptates? Commodi porro totam ea dicta animi!</p></SwiperSlide>
+        <SwiperSlide>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minima temporibus, aliquid est alias sint, vel debitis iste corporis magnam quasi neque fuga distinctio ipsum! Reiciendis, deleniti. A libero voluptate odio dolor suscipit unde eius sint ducimus facere ad vitae nobis consequuntur accusamus maiores atque, at dignissimos, nemo totam doloribus!</SwiperSlide>
+        <SwiperSlide>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minima temporibus, aliquid est alias sint, vel debitis iste corporis magnam quasi neque fuga distinctio ipsum! Reiciendis, deleniti. A libero voluptate odio dolor suscipit unde eius sint ducimus facere ad vitae nobis consequuntur accusamus maiores atque, at dignissimos, nemo totam doloribus!</SwiperSlide>
+        <SwiperSlide>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minima temporibus, aliquid est alias sint, vel debitis iste corporis magnam quasi neque fuga distinctio ipsum! Reiciendis, deleniti. A libero voluptate odio dolor suscipit unde eius sint ducimus facere ad vitae nobis consequuntur accusamus maiores atque, at dignissimos, nemo totam doloribus!</SwiperSlide>
+        <SwiperSlide>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minima temporibus, aliquid est alias sint, vel debitis iste corporis magnam quasi neque fuga distinctio ipsum! Reiciendis, deleniti. A libero voluptate odio dolor suscipit unde eius sint ducimus facere ad vitae nobis consequuntur accusamus maiores atque, at dignissimos, nemo totam doloribus!</SwiperSlide>
+        <SwiperSlide>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minima temporibus, aliquid est alias sint, vel debitis iste corporis magnam quasi neque fuga distinctio ipsum! Reiciendis, deleniti. A libero voluptate odio dolor suscipit unde eius sint ducimus facere ad vitae nobis consequuntur accusamus maiores atque, at dignissimos, nemo totam doloribus!</SwiperSlide>
+        <SwiperSlide>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minima temporibus, aliquid est alias sint, vel debitis iste corporis magnam quasi neque fuga distinctio ipsum! Reiciendis, deleniti. A libero voluptate odio dolor suscipit unde eius sint ducimus facere ad vitae nobis consequuntur accusamus maiores atque, at dignissimos, nemo totam doloribus!</SwiperSlide>
+        <SwiperSlide>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minima temporibus, aliquid est alias sint, vel debitis iste corporis magnam quasi neque fuga distinctio ipsum! Reiciendis, deleniti. A libero voluptate odio dolor suscipit unde eius sint ducimus facere ad vitae nobis consequuntur accusamus maiores atque, at dignissimos, nemo totam doloribus!</SwiperSlide>
+        <SwiperSlide>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minima temporibus, aliquid est alias sint, vel debitis iste corporis magnam quasi neque fuga distinctio ipsum! Reiciendis, deleniti. A libero voluptate odio dolor suscipit unde eius sint ducimus facere ad vitae nobis consequuntur accusamus maiores atque, at dignissimos, nemo totam doloribus!</SwiperSlide>
       </Swiper>
     </div>
   );

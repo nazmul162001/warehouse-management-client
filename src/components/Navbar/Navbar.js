07f1872f68f64c32ea01077ba27d713b46ea37cd
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-08 13:11:49 +0600

    responsive navbar using humberger menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,18 +18,25 @@ const Navbar = () => {
   };
 
   return (
-    <div className="px-10 py-5 sticky top-0 z-50 bg-white">
-
-      
-
-      
-      <nav className="flex justify-between items-center">
-        <div className="logo">
+    <div className="px-10 py-0 md:py-5 sticky top-0 z-50 bg-white">
+      <div
+        onClick={() => setOpen(!open)}
+        className="humberger-menu w-10 h-10 absolute top-3 right-3 md:hidden z-50"
+      >
+        {open ? <XIcon /> : <MenuAlt1Icon />}
+      </div>
+      <nav
+        className={`nav-container flex flex-col md:flex-row justify-start text-center md:justify-between bg-white
+         md:items-center pt-16 md:pt-0 w-9/12 md:w-full h-screen md:h-16 absolute top-0 md:static duration-300 ease-in-out ${
+            open ? 'left-0' : 'left-[-5000px]'
+          }`}
+      >
+        <div className="logo mx-auto md:mx-0">
           <Link to="/">
             <img src={logo} alt="" />
           </Link>
         </div>
-        <div className="nav-menu">
+        <div className="nav-menu flex flex-col md:flex-row">
           <NavLink
             className={({ isActive }) => (isActive ? 'active-link' : 'link')}
             to="/"
